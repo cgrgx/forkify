ecf7f9c7271fe01950ae7b2985a401dd348eafe3
chiran gurung <grgchiran08@yahoo.com>
2024-01-11 13:45:59 -0500

    implement search results

--- src/js/model.js
+++ src/js/model.js
@@ -15,7 +15,7 @@ export const state = {
 
 export const loadRecipe = async function (id) {
   try {
-    const data = await getJSON(`${API_URL}/${id}`);
+    const data = await getJSON(`${API_URL}${id}`);
 
     const { recipe } = data.data;
     state.recipe = {
@@ -29,7 +29,27 @@ export const loadRecipe = async function (id) {
       ingredients: recipe.ingredients,
     };
   } catch (err) {
-    console.error(`${err} 💥`);
+    console.error(`${err} 🔥🔥🔥`);
+    throw err;
+  }
+};
+
+export const loadSearchResults = async function (query) {
+  try {
+    state.search.query = query;
+    const data = await getJSON(`${API_URL}?search=${query}`);
+    console.log(data);
+
+    state.search.results = data.data.recipes.map(rec => {
+      return {
+        id: rec.id,
+        title: rec.title,
+        publisher: rec.publisher,
+        image: rec.image_url,
+      };
+    });
+  } catch (err) {
+    console.error(`${err} 🔥🔥🔥`);
     throw err;
   }
 };
