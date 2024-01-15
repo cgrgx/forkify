2eac6563bbd47544d33e60b36ed5dee9e3efdc57
chiran gurung <grgchiran08@yahoo.com>
2024-01-15 11:54:01 -0500

    implement pagination

--- src/js/config.js
+++ src/js/config.js
@@ -1,3 +1,4 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const API_KEY = '2fe95176-316d-41ee-b544-8c176010c15f';
 export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
