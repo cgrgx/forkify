2ffeb499c124e384c43484979f5d859e989a40c5
chiran gurung <grgchiran08@yahoo.com>
2024-01-31 11:38:20 -0500

    Added new welcome feature

--- src/js/controller.js
+++ src/js/controller.js
@@ -123,6 +123,10 @@ const controlAddRecipe = async function (newRecipe) {
   }
 };
 
+const newFeature = function () {
+  console.log('Welcome to the application!');
+};
+
 const init = function () {
   bookmarksView.addHandlerRender(controlBookmarks);
   recipeView.addHandlerRender(controlRecipes);
@@ -130,7 +134,7 @@ const init = function () {
   recipeView.addHandlerAddBookmark(controlAddBookmark);
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
-
   addRecipeView.addHandlerUpload(controlAddRecipe);
+  newFeature();
 };
 init();
